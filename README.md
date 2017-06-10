9f81091347b0669100a3c818f0def0a48571bccf
TillKoeln <Till_Noske@msn.com>
2017-06-10 18:02:25 +0200

    hello world

---
+++ README.md
@@ -0,0 +1,6 @@
+
+Bernie-Madoff development tree
+
+Bernie-Madoff is a PoS-based cryptocurrency.
+
+
---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define Bernie-Madoff-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "Bernie-MadoffURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
